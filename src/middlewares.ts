882f2e40812c6c0b1f6af3663a3141944cee5d3a
dedisalam <dedispermana20@gmail.com>
2022-11-06 14:35:04 +0700

    update

--- src/middlewares.ts
+++ src/middlewares.ts
@@ -66,6 +66,7 @@ class Middlewares {
     ];
     const corsOption: CorsOptions = {
       origin(origin, callback) {
+        console.log(origin);
         if (whitelist.indexOf(origin) !== -1) {
           callback(null, true);
         } else {
